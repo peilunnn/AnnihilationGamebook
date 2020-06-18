cf48a588ab7adc2170bbcceffd73f6a6a92e40de
peilunnn <61407534+peilunnn@users.noreply.github.com>
2020-06-18 21:29:34 +0800

    Add files via upload

---
+++ build/precache-manifest.9c9cd1da3b821cc7d62c718f8c31771b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1297ba89d2a64b37973e613bfe031545",
+    "url": "/CYOA-Annihilation/index.html"
+  },
+  {
+    "revision": "53a6e9341c9f0692f60f",
+    "url": "/CYOA-Annihilation/static/css/main.04f6a7b4.chunk.css"
+  },
+  {
+    "revision": "6f1df8d886f140cc3c24",
+    "url": "/CYOA-Annihilation/static/js/2.8f3ac4e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/CYOA-Annihilation/static/js/2.8f3ac4e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a6e9341c9f0692f60f",
+    "url": "/CYOA-Annihilation/static/js/main.21a83cfb.chunk.js"
+  },
+  {
+    "revision": "dda6915b870b3ce0028d",
+    "url": "/CYOA-Annihilation/static/js/runtime-main.925d3efc.js"
+  }
+]);
